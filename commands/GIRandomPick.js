aa311a6a9546022e58a4c787520b9cf3cd3b5118
Truong Vinh Tran <truongvinht@gmail.com>
2023-02-08 19:08:43 +0100

    another test

--- commands/GIRandomPick.js
+++ commands/GIRandomPick.js
@@ -9,11 +9,54 @@ class GIRandomPick {
     this.res = res;
   }
 
-  async fetchData(apiServer) {
-    return await fetch(`${apiServer}/api/yuanshen/elements`);
-  }
-
   send(interaction, apiServer) {
+    fetch(`${apiServer}/api/yuanshen/elements`)
+      .then((response) => {
+        let types = undefined;
+        let elements = undefined;
+
+        const data = response.json().data;
+
+        elements = [];
+        for (let obj in data) {
+          elements.push(obj.name);
+        }
+        console.log(JSON.stringify(elements));
+
+        types = data.types;
+
+        let pickedType = Math.floor(Math.random() * Math.floor(types.length));
+        let pickedElement = Math.floor(
+          Math.random() * Math.floor(elements.length)
+        );
+        let pickedRate = Math.floor(Math.random() * Math.floor(2)) + 4;
+
+        return this.res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: `${interaction.member.user.username}, suche dir eine ${elements[pickedElement]}-Figur aus, die ${types[pickedType]} und ${pickedRate}-Sterne ist.`,
+          },
+        });
+      })
+      .catch((_) => {
+        let types = undefined;
+        let elements = undefined;
+        elements = data.elements;
+        types = data.types;
+
+        let pickedType = Math.floor(Math.random() * Math.floor(types.length));
+        let pickedElement = Math.floor(
+          Math.random() * Math.floor(elements.length)
+        );
+        let pickedRate = Math.floor(Math.random() * Math.floor(2)) + 4;
+
+        return this.res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: `${interaction.member.user.username}, suche dir eine ${elements[pickedElement]}-Figur aus, die ${types[pickedType]} und ${pickedRate}-Sterne ist.`,
+          },
+        });
+      });
 
     // fetch
     const response = this.fetchData(apiServer);
@@ -26,7 +69,6 @@ class GIRandomPick {
       elements = data.elements;
       console.log(JSON.stringify(response));
     } else {
-      
       const data = response.data;
 
       elements = [];
