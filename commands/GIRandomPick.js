a6bc5a1b44c74167a1fff99d75a73bcf30256b8d
Truong Vinh Tran <truongvinht@gmail.com>
2023-02-08 18:29:31 +0100

    use gaming service for data input

--- commands/GIRandomPick.js
+++ commands/GIRandomPick.js
@@ -1,29 +1,49 @@
-
-const { InteractionResponseType } = require('discord-interactions');
+const { json } = require("body-parser");
+const { InteractionResponseType } = require("discord-interactions");
 const LogHelper = require("../loaders/loghelper");
 const logger = LogHelper.getInstance();
-const data = require("./figures.json")
+const data = require("./figures.json");
 
 class GIRandomPick {
-  static cmd = 'rngpick';
+  static cmd = "rngpick";
   constructor(res) {
     this.res = res;
   }
 
   send(interaction) {
-    const types = data.types;
-    const elements = data.elements;
+    const PUBLIC_KEY = process.env.API_SERVER || "localhost:3000";
+    // fetch
+    const response = fetch(`${PUBLIC_KEY}/api/yuanshen/elements`);
+
+    let types = undefined;
+    let elements = undefined;
+
+    if (!response.ok) {
+      // fallback local json res
+      elements = data.elements;
+    } else {
+      
+      const data = response.data;
+
+      elements = [];
+      for (let obj in data) {
+        elements.push(obj.name);
+      }
+      console.log(JSON.stringify(elements));
+    }
+
+    types = data.types;
 
     let pickedType = Math.floor(Math.random() * Math.floor(types.length));
     let pickedElement = Math.floor(Math.random() * Math.floor(elements.length));
-    let pickedRate = Math.floor(Math.random() * Math.floor(2)) + 4; 
+    let pickedRate = Math.floor(Math.random() * Math.floor(2)) + 4;
 
     return this.res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `${interaction.member.user.username}, suche dir eine ${elements[pickedElement]}-Figur aus, die ${types[pickedType]} und ${pickedRate}-Sterne ist.`,
-        },
-      });
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: `${interaction.member.user.username}, suche dir eine ${elements[pickedElement]}-Figur aus, die ${types[pickedType]} und ${pickedRate}-Sterne ist.`,
+      },
+    });
   }
 }
 
