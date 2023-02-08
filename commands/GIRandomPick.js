b5b101ca70f4b6639de97f46cc66f9643803de3f
Truong Vinh Tran <truongvinht@gmail.com>
2023-02-08 19:10:14 +0100

    remove unused code

--- commands/GIRandomPick.js
+++ commands/GIRandomPick.js
@@ -57,39 +57,6 @@ class GIRandomPick {
           },
         });
       });
-
-    // fetch
-    const response = this.fetchData(apiServer);
-
-    let types = undefined;
-    let elements = undefined;
-
-    if (!response.ok) {
-      // fallback local json res
-      elements = data.elements;
-      console.log(JSON.stringify(response));
-    } else {
-      const data = response.data;
-
-      elements = [];
-      for (let obj in data) {
-        elements.push(obj.name);
-      }
-      console.log(JSON.stringify(elements));
-    }
-
-    types = data.types;
-
-    let pickedType = Math.floor(Math.random() * Math.floor(types.length));
-    let pickedElement = Math.floor(Math.random() * Math.floor(elements.length));
-    let pickedRate = Math.floor(Math.random() * Math.floor(2)) + 4;
-
-    return this.res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: `${interaction.member.user.username}, suche dir eine ${elements[pickedElement]}-Figur aus, die ${types[pickedType]} und ${pickedRate}-Sterne ist.`,
-      },
-    });
   }
 }
 
