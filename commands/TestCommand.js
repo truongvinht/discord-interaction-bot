e009618f0952de73445fce2095bb1ea2f03bc401
Truong Vinh Tran <truongvinht@gmail.com>
2023-02-13 20:55:20 +0100

    adjust test cmd

--- commands/TestCommand.js
+++ commands/TestCommand.js
@@ -1,19 +1,75 @@
-
-const { InteractionResponseType } = require('discord-interactions');
+const { InteractionResponseType } = require("discord-interactions");
+const GamingService = require("../services/GamingService");
 
 class TestCommand {
-  static cmd = 'test';
+  static cmd = "test";
   constructor(res) {
     this.res = res;
   }
 
   send(message) {
-    return this.res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `<@${message.member.user.id}>: ${message.data.options[0].value}.`,
-        },
-      });
+    const callback = (response, error) => {
+      if (error != null) {
+        this.res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: `ERROR - Server is down...`,
+          },
+        });
+      } else {
+        const figures = response.data;
+
+        if (Object.prototype.hasOwnProperty(message.data, "options")) {
+          const pickedFigures = [];
+
+          let text =  `<@${message.member.user.id}>, spiele mal \n`;
+          let textUrl = ``;
+
+          do {
+            const random = Math.floor(Math.random() * figures.length);
+            if (!pickedFigures.includes(random)) {
+                //pickedFigures.push(random);
+                const name = figures[random].name;
+                const url = figures[random].image_url;
+                textUrl = `${textUrl} ${url}`;
+                text = `${text} -${name}\n`;
+            }
+          } while (pickedFigures.length < message.data.options[0].value);
+
+          this.res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `${text} \n${textUrl}`,
+            },
+          });
+
+        } else {
+          const pickedFigure = Math.floor(
+            Math.random() * Math.floor(figures.length)
+          );
+
+          // get the name
+          const name = figures[pickedFigure].name;
+          const url = figures[pickedFigure].image_url;
+
+          this.res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `<@${message.member.user.id}>, spiele mal ${name}. \n(${url})`,
+            },
+          });
+        }
+      }
+    };
+
+    const service = new GamingService();
+    service.fetchAllFigures(callback);
+    // return this.res.send({
+    //     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    //     data: {
+    //       content: `<@${message.member.user.id}>: ${message.data.options[0].value}.`,
+    //     },
+    //   });
   }
 }
 
