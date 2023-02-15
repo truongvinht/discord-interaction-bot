b6da09e4151f79e56a9177c3bf9e608d1bee10db
Truong Vinh Tran <truongvinht@gmail.com>
2023-02-15 16:15:32 +0100

    prevent duplicates

--- commands/GIAbyss.js
+++ commands/GIAbyss.js
@@ -1,27 +1,35 @@
-const { InteractionResponseType } = require("discord-interactions");
-const GamingService = require("../services/GamingService");
+const { InteractionResponseType } = require('discord-interactions');
+const GamingService = require('../services/GamingService');
 
 class TestCommand {
-  static cmd = "abyss";
+  static cmd = 'abyss';
   constructor(res) {
     this.res = res;
   }
 
   send(message) {
     const callback = (response, error) => {
+      if (error != null) {
+        this.res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: `ERROR - Server is down...`,
+          },
+        });
+      } else {
         const figures = response.data;
         const pickedFigures = [];
 
-        let text =  `<@${message.member.user.id}>, spiele im Abgrund mit nur Figuren aus dieser Liste:\n`;
+        let text = `<@${message.member.user.id}>, spiele im Abgrund mit nur Figuren aus dieser Liste:\n`;
 
         do {
           const random = Math.floor(Math.random() * figures.length);
           if (!pickedFigures.includes(random)) {
-              const name = figures[random].name;
-              text = `${text} - ${name}\n`;
-              pickedFigures.push(figures[random]);
+            const name = figures[random].name;
+            text = `${text} - ${name}\n`;
+            pickedFigures.push(random);
           }
-        } while (pickedFigures.length < 11);
+        } while (pickedFigures.length < 10);
 
         this.res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -29,6 +37,7 @@ class TestCommand {
             content: `${text}`,
           },
         });
+      }
     };
 
     const service = new GamingService();
